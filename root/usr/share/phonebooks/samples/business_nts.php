6425aefad52d3ee377e8b883d8e06e402ec65935
Stefano Fancello <stefano.fancello@nethesis.it>
2019-03-28 15:19:49 +0100

    Business sample: fix indentation

--- root/usr/share/phonebooks/samples/business_nts.php
+++ root/usr/share/phonebooks/samples/business_nts.php
@@ -9,40 +9,40 @@ mysql_select_db('phonebook', $database);
 mysql_set_charset("utf8");
 
 function ODBCconnect(){
-     //Modifica i seguenti parametri di accesso all'odbc
-     $dsn="business";
-     $user="sa";
-     $pass="nts";
+    //Modifica i seguenti parametri di accesso all'odbc
+    $dsn="business";
+    $user="sa";
+    $pass="nts";
 
-     global $ODBC_handle;
-     if (!isset($ODBC_handle)){
-         $ODBC_handle = odbc_connect($dsn,$user,$pass);
-     }
-     if (!isset($ODBC_handle)) die("ODBCconnect() failed!");
+    global $ODBC_handle;
+    if (!isset($ODBC_handle)){
+        $ODBC_handle = odbc_connect($dsn,$user,$pass);
+    }
+    if (!isset($ODBC_handle)) die("ODBCconnect() failed!");
  }
 
- //Questa funzione definisce la connessione con il DSN creato precedentemente
+//Questa funzione definisce la connessione con il DSN creato precedentemente
 
- function ODBCquery($query){
-     ODBCconnect();
-     global $ODBC_handle;
-     return odbc_exec($ODBC_handle,$query);
- }
+function ODBCquery($query){
+    ODBCconnect();
+    global $ODBC_handle;
+    return odbc_exec($ODBC_handle,$query);
+}
 
- //Questa funzione esegue la query su MSSQLSERVER tramite l'odbc
+//Questa funzione esegue la query su MSSQLSERVER tramite l'odbc
 
- function ODBCquery2array($query){
-     $result = ODBCquery($query);
-     while ($row = odbc_fetch_array($result)){
-         $returnarray[] = $row;
-     }
-     if (isset($returnarray)) return $returnarray; else return false;
- }
+function ODBCquery2array($query){
+    $result = ODBCquery($query);
+    while ($row = odbc_fetch_array($result)){
+        $returnarray[] = $row;
+    }
+    if (isset($returnarray)) return $returnarray; else return false;
+}
 
- //Query da modificare secondo le esigenze specifiche del server usato
- # ATTENZIONE!! è necessario fare il cast() dei campi di testo per evitare l'errore "Out of Memory"
+//Query da modificare secondo le esigenze specifiche del server usato
+# ATTENZIONE!! è necessario fare il cast() dei campi di testo per evitare l'errore "Out of Memory"
 
- $query="select cast(an_descr1 as varchar(255)) as azienda1, cast(an_descr2 as varchar(255)) as azienda2,
+$query="select cast(an_descr1 as varchar(255)) as azienda1, cast(an_descr2 as varchar(255)) as azienda2,
 		cast(an_contatt as varchar(255)) as contatto,
 		an_telef as tel,
 		an_email as email,
@@ -54,50 +54,50 @@ function ODBCconnect(){
 		an_cap as cap
 	from ANAGRA where an_tipo = 'c' or an_tipo = 'f';";
 
- $rubrica_ext = ODBCquery2array($query);
+$rubrica_ext = ODBCquery2array($query);
 
 // Remove Business contacts from centralized phonebook
 mysql_query('DELETE FROM phonebook WHERE sid_imported = "business"',$database);
 
 
- foreach ($rubrica_ext as $record) {
-        $azienda=$record['azienda1'].' '.$record['azienda2'];
-        $nome=$record['contatto'];
-        $email=$record['email'];
-        $via=$record['via'];
-        $citta=$record['citta'];
-        $prov=$record['prov'];
-        $cap=$record['cap'];
-        $cell=$record['cell'];
-        $tel=str_replace("-","",$record['tel']);
-        $tel=str_replace(" ","",$tel);
-        $tel=str_replace("/","",$tel);
-        $tel=str_replace("+","00",$tel);
-       	$fax=str_replace("-","",$record['fax']);
-        $fax=str_replace(" ","",$fax);
-        $fax=str_replace("/","",$fax);
-        $fax=str_replace("+","00",$fax);
-		    $cell=str_replace("-","",$record['cell']);
-        $cell=str_replace(" ","",$cell);
-        $cell=str_replace("/","",$cell);
-        $cell=str_replace("+","00",$cell);
+foreach ($rubrica_ext as $record) {
+    $azienda=$record['azienda1'].' '.$record['azienda2'];
+    $nome=$record['contatto'];
+    $email=$record['email'];
+    $via=$record['via'];
+    $citta=$record['citta'];
+    $prov=$record['prov'];
+    $cap=$record['cap'];
+    $cell=$record['cell'];
+    $tel=str_replace("-","",$record['tel']);
+    $tel=str_replace(" ","",$tel);
+    $tel=str_replace("/","",$tel);
+    $tel=str_replace("+","00",$tel);
+    $fax=str_replace("-","",$record['fax']);
+    $fax=str_replace(" ","",$fax);
+    $fax=str_replace("/","",$fax);
+    $fax=str_replace("+","00",$fax);
+    $cell=str_replace("-","",$record['cell']);
+    $cell=str_replace(" ","",$cell);
+    $cell=str_replace("/","",$cell);
+    $cell=str_replace("+","00",$cell);
 
  	$query_ins = "INSERT INTO phonebook  SET
- 			company='".mysql_escape_string($azienda)."',
-			name='".mysql_escape_string($nome)."',
-			workphone='".mysql_escape_string($tel)."',
-			fax='".mysql_escape_string($fax)."',
-			workemail='".mysql_escape_string($email)."',
-			workstreet='".mysql_escape_string($via)."',
-			workcity='".mysql_escape_string($citta)."',
-			workprovince='".mysql_escape_string($prov)."',
-			workpostalcode='".mysql_escape_string($cap)."',
-            cellphone='".mysql_escape_string($cell)."',
-            type='business',
-            sid_imported='business';";
+ 	    company='".mysql_escape_string($azienda)."',
+		name='".mysql_escape_string($nome)."',
+		workphone='".mysql_escape_string($tel)."',
+		fax='".mysql_escape_string($fax)."',
+		workemail='".mysql_escape_string($email)."',
+		workstreet='".mysql_escape_string($via)."',
+		workcity='".mysql_escape_string($citta)."',
+		workprovince='".mysql_escape_string($prov)."',
+		workpostalcode='".mysql_escape_string($cap)."',
+        cellphone='".mysql_escape_string($cell)."',
+        type='business',
+        sid_imported='business';";
 
- 	$result = mysql_query($query_ins,$database);
- }
+    $result = mysql_query($query_ins,$database);
+}
 
 
 
