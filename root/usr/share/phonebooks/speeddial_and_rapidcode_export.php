0efd92c61a7c3f12b1a49396c2ea990e3cdedb01
Stefano <stefano.fancello@nethesis.it>
2019-05-23 18:02:15 +0200

    Update speeddial_and_rapidcode_export.php
    Make sure speed dials and rapidcodes are deleted when phonebook is refreshed

--- root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
+++ root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
@@ -21,7 +21,7 @@ $dhost2 = 'localhost';
 $db2 = new PDO("mysql:host=$dhost2;dbname=phonebook",$duser2, $dpass2);
 
 // Remove NethVoice extensions from centralized phonebook
-$db2->query('DELETE FROM phonebook WHERE sid_imported = "NethVoice RapidCodes"');
+$db2->query('DELETE FROM phonebook WHERE sid_imported = "NethVoice RapidCodes" OR sid_imported = "speeddial"');
 
 // Export Speed Dials
 try {
@@ -76,7 +76,7 @@ try {
                                                 fax, title, company, notes, name, homestreet, homepob, homecity,
                                                 homeprovince, homepostalcode, homecountry, workstreet, workpob,
                                                 workcity, workprovince, workpostalcode, workcountry, url, sid_imported)
-                      VALUES ('admin', 'rapidcode', '', '', '',?, '', '', '', '','', ?, '', '', '','', '', '', '','', '', '', '','', '', 'speeddial')";
+                      VALUES ('admin', 'rapidcode', '', '', '',?, '', '', '', '','', ?, '', '', '','', '', '', '','', '', '', '','', '', 'NethVoice RapidCodes')";
             $stmt2 = $db2->prepare($query);
             $stmt2->execute(array($row["number"],$row["label"]));
         }
