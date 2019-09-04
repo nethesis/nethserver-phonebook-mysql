73f6589ae81073f4b2439d8c9ead798c974abf16
Stefano Fancello <stefano.fancello@nethesis.it>
2019-09-04 09:13:51 +0200

    New sample scripts for Business and Gamma Enterprise

--- root/usr/share/phonebooks/samples/gamma_enterprise.php
+++ root/usr/share/phonebooks/samples/gamma_enterprise.php
@@ -1,81 +1,96 @@
 #!/usr/bin/php -q
 <?php
+/****************************
+*  Gamma DB credentials  *
+*****************************/
+$dsn="teamsystem";
+$user="sa";
+$pass="Teamsystem01";
+/****************************/
 
-exec('perl -e \'use NethServer::Password; my $password = NethServer::Password::store(\'PhonebookDBPasswd\')  ; printf $password;\'',$out); //get sogo db password
+$code = 0;
+
+// Get NethServer phonebook database credentials
+exec('perl -e \'use NethServer::Password; my $password = NethServer::Password::store(\'PhonebookDBPasswd\')  ; printf $password;\'',$out);
 $pbookpass = $out[0];
 
-$database = mysql_connect('localhost','pbookuser',$pbookpass) or die("Database error config");
-mysql_select_db('phonebook', $database);
-mysql_set_charset("utf8");
+// Connect to NethServer phonebook database
+$phonebookDB = new PDO(
+    'mysql:host=localhost;dbname=phonebook',
+    'pbookuser',
+    $pbookpass);
+$phonebookDB->exec("set names utf8");
 
- function ODBCconnect(){ 
-     //Modifica i seguenti parametri di accesso all'odbc
-     $dsn="DSN_NAME";
-     $user="dsn_user";
-     $pass="dsn_pass";
+// Connect to MSSQL using PDO odbc driver
+$mssqlDB = new PDO(
+    'odbc:'.$dsn,
+    $user,
+    $pass);
+$phonebookDB->exec("set names utf8");
 
-     global $ODBC_handle;
-     if (!isset($ODBC_handle)){
-         $ODBC_handle = odbc_connect($dsn,$user,$pass);
-     }
-     if (!isset($ODBC_handle)) die("ODBCconnect() failed!");
- }
+// Remove Business contacts from centralized phonebook
+try {
+    $phonebookDB->exec('DELETE FROM phonebook WHERE sid_imported = "gamma"');
+} catch (Exception $e) {
+    echo "Error '".$e->getMessage()."' cleaning phonebook";
+    $code ++;
+}
+//Query da modificare secondo le esigenze specifiche del server usato
+# ATTENZIONE!! è necessario fare il cast() dei campi di testo per evitare l'errore "Out of Memory"
+#$query="select cast(CG16_RAGSOANAG as varchar(255)) as azienda, cast(CG16_COGNOME as varchar(255)) as cognome, cast(CG16_NOME as varchar(255)) as nome, CG16_TEL1NUM as tel, CG16_INDEMAIL as email, CG16_FAXNUM as fax, cast(CG16_INDIRIZZO as varchar(255)) as via, cast(CG16_CITTA as varchar(255)) as citta, CG16_PROV as prov, CG16_CAP as cap from CG16_ANAGGEN";
+$query="select CG16_RAGSOANAG as azienda, CG16_COGNOME as cognome, CG16_NOME as nome, CG16_TEL1NUM as tel, CG16_INDEMAIL as email, CG16_FAXNUM as fax, CG16_INDIRIZZO as via, CG16_CITTA as citta, CG16_PROV as prov, CG16_CAP as cap from CG16_ANAGGEN";
 
- //Questa funzione definisce la connessione con il DNS creato precedentemente
+try {
+    $sth = $mssqlDB->prepare($query);
+    $sth->execute(array());
+} catch (Exception $e) {
+    echo "Error '".$e->getMessage()."' executing query: $query";
+    $code ++;
+}
+while ($record = $sth->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)) {
+    $azienda = (isset($record['azienda']) ? $record['azienda'] : '');
+    $nome = (isset($record['cognome']) ? $record['cognome'] : '')." ".$record['nome'];
+    $email = (isset($record['email']) ? $record['email'] : '');
+    $via = (isset($record['via']) ? $record['via'] : '');
+    $citta = (isset($record['citta']) ? $record['citta'] : '');
+    $prov = (isset($record['prov']) ? $record['prov'] : '');
+    $cap = (isset($record['cap']) ? $record['cap'] : '');
+    $workcountry = (isset($record['workcountry']) ? $record['workcountry'] : '');
+    foreach (['tel','fax','cell','homephone'] as $field) {
+        if (isset($record[$field])) {
+            $$field = preg_replace("/-| |\//","",$record[$field]);
+            $$field = str_replace("+","00",$$field);
+        } else {
+            $$field = '';
+        }
+    }
 
- function ODBCquery($query){
-     ODBCconnect();
-     global $ODBC_handle;
-     return odbc_exec($ODBC_handle,$query);
- }
+    $query_ins = "INSERT INTO phonebook
+        (company,workstreet,workcity,workprovince,workphone,homephone,cellphone,fax,workemail,workcountry,type,sid_imported)
+        VALUES
+        (?,?,?,?,?,?,?,?,?,?,?,?)";
 
- //Questa funzione esegue la query su MSSQLSERVER tramite l'odbc
+    try {
+        $sth2 = $phonebookDB->prepare($query_ins);
+        $sth2->execute(array(
+            $azienda,
+            $via,
+            $citta,
+            $prov,
+            $tel,
+            $homephone,
+            $cell,
+            $fax,
+            $email,
+            $workcountry,
+            'gamma',
+            'gamma'
+        ));
+    } catch (Exception $e) {
+        echo "Error '".$e->getMessage()."' executing query: $query_ins";
+        $code ++;
+    }
+}
 
- function ODBCquery2array($query){
-     $result = ODBCquery($query);
-     while ($row = odbc_fetch_array($result)){
-         $returnarray[] = $row;
-     }
-     if (isset($returnarray)) return $returnarray; else return false;
- }
-
- //Quest'ultima utilizza la funzione precedente, e distribuisce il risultato in un array
- # ATTENZIONE!! è necessario fare il cast() dei campi di testo per evitare l'errore "Out of Memory"
-
- $query="select cast(CG16_RAGSOANAG as varchar(255)) as azienda, cast(CG16_COGNOME as varchar(255)) as cognome, cast(CG16_NOME as varchar(255)) as nome, CG16_TEL1NUM as tel, CG16_INDEMAIL as email, CG16_FAXNUM as fax, cast(CG16_INDIRIZZO as varchar(255)) as via, cast(CG16_CITTA as varchar(255)) as citta, CG16_PROV as prov, CG16_CAP as cap from CG16_ANAGGEN;";
- $rubrica_ext = ODBCquery2array($query);
-
- foreach ($rubrica_ext as $record) {
-        $azienda=$record['azienda'];
-        $nome=$record['cognome']." ".$record['nome'];
-        $email=$record['email'];
-        $via=$record['via'];
-        $citta=$record['citta'];
-        $prov=$record['prov'];
-        $cap=$record['cap'];
-        $cell=$record['cell'];
-        $tel=str_replace("-","",$record['tel']);
-        $tel=str_replace(" ","",$tel);
-        $tel=str_replace("/","",$tel);
-        $tel=str_replace("+","00",$tel);
-       	$fax=str_replace("-","",$record['fax']);
-        $fax=str_replace(" ","",$fax);
-        $fax=str_replace("/","",$fax);
-        $fax=str_replace("+","00",$fax);
-
-        $query_ins = "INSERT INTO phonebook  SET 
-                        company='".mysql_escape_string($azienda)."', 
-                        name='".mysql_escape_string($nome)."', 
-                        workphone='".mysql_escape_string($tel)."', 
-                        fax='".mysql_escape_string($fax)."', 
-                        workemail='".mysql_escape_string($email)."', 
-                        workstreet='".mysql_escape_string($via)."', 
-                        workcity='".mysql_escape_string($citta)."', 
-                        workprovince='".mysql_escape_string($prov)."', 
-                        workpostalcode='".mysql_escape_string($cap)."', 
-                        cellphone='".mysql_escape_string($cell)."';";
-
- 	$result = mysql_query($query_ins,$database);
- }
- 
+exit($code);
 
