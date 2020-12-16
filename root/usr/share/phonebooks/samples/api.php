5dc4c60957404d33365183ccd4d0f6835324cd5c
Stefano Fancello <stefano.fancello@nethesis.it>
2020-12-16 16:36:14 +0100

    Verify remote certificate

--- root/usr/share/phonebooks/samples/api.php
+++ root/usr/share/phonebooks/samples/api.php
@@ -11,7 +11,6 @@ $ch = curl_init();
 */
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Content-Type: application/json",
     ));
