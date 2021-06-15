9229d30ce5732d3967c801ce57147bce4b44a56c
Stefano Fancello <stefano.fancello@nethesis.it>
2021-06-15 11:08:33 +0200

    Enforcce sizeLimit LDAP property

--- root/usr/share/phonebookjs/phonebook.js
+++ root/usr/share/phonebookjs/phonebook.js
@@ -213,7 +213,7 @@ db.query("SELECT name,company,homephone,workphone,cellphone,fax FROM phonebook",
     req.filter = lowercaseSearchParameters(req.filter);
     for (var i = 0; i < userinfo[binddn].addrbooks.length; i++) {
       if (req.filter.matches(userinfo[binddn].addrbooks[i].attributes)) {
-        if (config.limit > 0 && sent >= config.limit) {
+        if ((config.limit > 0 && sent >= config.limit) || (req.sizeLimit > 0 && i >= req.sizeLimit)) {
             break;
         } else {
             res.send(userinfo[binddn].addrbooks[i]);
