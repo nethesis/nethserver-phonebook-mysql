ecfd80dab7512265c15ee8c9504821a51ee17c4f
Stefano Fancello <stefano.fancello@nethesis.it>
2023-04-14 18:04:38 +0200

    Fix extensions export query

--- root/usr/share/phonebooks/nethvoice_extensions_export.php
+++ root/usr/share/phonebooks/nethvoice_extensions_export.php
@@ -31,50 +31,48 @@ $sth->execute([]);
 $sth = $nethvoicedb->prepare('SELECT default_extension as extension,displayname as name FROM userman_users WHERE default_extension != "none"');
 $sth->execute([]);
 
+$query = 'INSERT INTO phonebook (
+	owner_id,
+        type,
+	homeemail,
+	workemail,
+	homephone,
+	workphone,
+	cellphone,
+	fax,
+	title,
+	company,
+	notes,
+	name,
+	homestreet,
+	homepob,
+	homecity,
+	homeprovince,
+	homepostalcode,
+	homecountry,
+	workstreet,
+	workpob,
+	workcity,
+	workprovince,
+	workpostalcode,
+	workcountry,
+	url,
+	sid_imported
+) VALUES ';
+
 $qm = [];
+$query_values = [];
 while($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
         if($DEBUG) {
                 print_r($row);
 	}
-	if (empty($query)) { 
-	        $query = 'INSERT INTO phonebook.phonebook (
-                                owner_id,
-                                type,
-                                homeemail,
-                                workemail,
-                                homephone,
-                                workphone,
-                                cellphone,              
-                                fax,
-                                title,
-                                company,
-                                notes,
-                                name,
-                                homestreet,
-                                homepob,
-                                homecity,               
-                                homeprovince,
-                                homepostalcode,
-                                homecountry,
-                                workstreet,
-                                workpob,                
-                                workcity,
-                                workprovince,
-                                workpostalcode,
-                                workcountry,
-                                url,
-                                sid_imported
-                        )
-			VALUES ';
-	}
-
-	$query .= '("admin", "extension", "", "", "",? , "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "nethvoice extensions")';
+	$query_values[] = '("admin", "extension", "", "", "",? , "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "nethvoice extensions")';
 	$qm[] = $row['extension'];
 	$qm[] = $row['name'];
 }
 
 if (!empty($qm)) {
-	$sth = $nethvoicedb->prepare($query);
+	$sth = $phonebookdb->prepare($query.implode(',',$query_values));
 	$sth->execute($qm);
 }
 
