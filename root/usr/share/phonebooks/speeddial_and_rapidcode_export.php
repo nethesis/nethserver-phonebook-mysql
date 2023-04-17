d97d3dc020d5c395eff5b20128be92c20bbbe41b
Stefano Fancello <stefano.fancello@nethesis.it>
2023-04-17 15:57:04 +0200

    Fix speeddial export query

--- root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
+++ root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
@@ -32,50 +32,51 @@ $sth->execute([]);
 $sth = $nethvoicedb->prepare('SELECT number AS extension, label AS name from `phonebook` order by name ');
 $sth->execute([]);
 
+$query = 'INSERT INTO phonebook.phonebook (
+	owner_id,
+	type,
+	homeemail,
+	workemail,
+	homephone,
+	workphone,
+	cellphone,
+	fax,
+	title,
+	company,
+	notes,
+	name,
+	homestreet,
+	homepob,
+	homecity,
+	homeprovince,
+	homepostalcode,
+	homecountry,
+	workstreet,
+	workpob,
+	workcity,
+	workprovince,
+	workpostalcode,
+	workcountry,
+	url,
+	sid_imported
+) VALUES ';
+
 $qm = [];
+$query_values = [];
+
 while($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
-        if($DEBUG) {
-                print_r($row);
-        }
-        if (empty($query)) {
-                $query = 'INSERT INTO phonebook.phonebook (
-                                owner_id,
-                                type,
-                                homeemail,
-                                workemail,
-                                homephone,
-                                workphone,
-                                cellphone,
-                                fax,
-                                title,
-                                company,
-                                notes,
-                                name,
-                                homestreet,
-                                homepob,
-                                homecity,
-                                homeprovince,
-                                homepostalcode,
-                                homecountry,
-                                workstreet,
-                                workpob,
-                                workcity,
-                                workprovince,
-                                workpostalcode,
-                                workcountry,
-                                url,
-                                sid_imported
-                        )
-			VALUES ';
+	if($DEBUG) {
+		print_r($row);
 	}
-	$query .= '("admin", "speeddial", "", "", "", ?, "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "speeddial")';
+
+	$query_values[] = '("admin", "speeddial", "", "", "", ?, "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "speeddial")';
 	$qm[] = $row['extension'];
 	$qm[] = $row['name'];
 }
 
 if (!empty($qm)) {
-        $sth = $nethvoicedb->prepare($query);
-        $sth->execute($qm);
+	$sth = $phonebookdb->prepare($query.implode(',',$query_values));
+    $sth->execute($qm);
 }
 
 
@@ -83,49 +84,49 @@ if (!empty($qm)) {
 $sth = $nethvoicedb->prepare('SELECT number AS extension, label AS name from `phonebook` order by name ');
 $sth->execute([]);
 
+
+$query = 'INSERT INTO phonebook.phonebook (
+	owner_id,
+	type,
+	homeemail,
+	workemail,
+	homephone,
+	workphone,
+	cellphone,
+	fax,
+	title,
+	company,
+	notes,
+	name,
+	homestreet,
+	homepob,
+	homecity,
+	homeprovince,
+	homepostalcode,
+	homecountry,
+	workstreet,
+	workpob,
+	workcity,
+	workprovince,
+	workpostalcode,
+	workcountry,
+	url,
+	sid_imported
+) VALUES ';
+
 $qm = [];
+$sth = $phonebookdb->prepare($query.implode(',',$query_values));
 while($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
-        if($DEBUG) {
-                print_r($row);
-        }
-        if (empty($query)) {
-                $query = 'INSERT INTO phonebook.phonebook (
-                                owner_id,
-                                type,
-                                homeemail,
-                                workemail,
-                                homephone,
-                                workphone,
-                                cellphone,
-                                fax,
-                                title,
-                                company,
-                                notes,
-                                name,
-                                homestreet,
-                                homepob,
-                                homecity,
-                                homeprovince,
-                                homepostalcode,
-                                homecountry,
-                                workstreet,
-                                workpob,
-                                workcity,
-                                workprovince,
-                                workpostalcode,
-                                workcountry,
-                                url,
-                                sid_imported
-                        )
-			VALUES ';
-	}
-	$query .= '("admin", "rapidcode", "", "", "", ?, "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "NethVoice RapidCodes")';
+    if($DEBUG) {
+        print_r($row);
+    }
+
+	$query_values[] = '("admin", "rapidcode", "", "", "", ?, "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "NethVoice RapidCodes")';
 	$qm[] = $row['extension'];
 	$qm[] = $row['name'];
 }
 
 if (!empty($qm)) {
-        $sth = $nethvoicedb->prepare($query);
-        $sth->execute($qm);
+	$sth = $phonebookdb->prepare($query.implode(',',$query_values));
+    $sth->execute($qm);
 }
-
