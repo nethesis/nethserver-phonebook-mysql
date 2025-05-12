5035eb8315a0aaad19e879c6c6088f71244a51f5
Stefano <gentoo.stefano@gmail.com>
2025-05-12 14:57:34 +0200

    fix(rapidcode): Fix import rapidcode into cetralized phonebook

--- root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
+++ root/usr/share/phonebooks/speeddial_and_rapidcode_export.php
@@ -28,60 +28,8 @@ $phonebookdb = new PDO(
 $sth = $phonebookdb->prepare('DELETE FROM phonebook WHERE sid_imported = "NethVoice RapidCodes" OR sid_imported = "speeddial"');
 $sth->execute([]);
 
-// Import Speed dials
-$sth = $nethvoicedb->prepare('SELECT number AS extension, label AS name from `phonebook` order by name ');
-$sth->execute([]);
-
-$query = 'INSERT INTO phonebook.phonebook (
-	owner_id,
-	type,
-	homeemail,
-	workemail,
-	homephone,
-	workphone,
-	cellphone,
-	fax,
-	title,
-	company,
-	notes,
-	name,
-	homestreet,
-	homepob,
-	homecity,
-	homeprovince,
-	homepostalcode,
-	homecountry,
-	workstreet,
-	workpob,
-	workcity,
-	workprovince,
-	workpostalcode,
-	workcountry,
-	url,
-	sid_imported
-) VALUES ';
-
-$qm = [];
-$query_values = [];
-
-while($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
-	if($DEBUG) {
-		print_r($row);
-	}
-
-	$query_values[] = '("admin", "speeddial", "", "", "", ?, "", "", "", "","", ?, "", "", "", "", "", "", "", "", "", "", "", "", "", "speeddial")';
-	$qm[] = $row['extension'];
-	$qm[] = $row['name'];
-}
-
-if (!empty($qm)) {
-	$sth = $phonebookdb->prepare($query.implode(',',$query_values));
-    $sth->execute($qm);
-}
-
-
 // Import NethVoice Rapidcode
-$sth = $nethvoicedb->prepare('SELECT number AS extension, label AS name from `phonebook` order by name ');
+$sth = $nethvoicedb->prepare('SELECT number AS extension, label AS name from `rapidcode` order by name ');
 $sth->execute([]);
 
 
